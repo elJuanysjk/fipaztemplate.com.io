8cd151c82d23a0834a2f64efd4b0387bb26187bc
elJuanysjk <92630468+elJuanysjk@users.noreply.github.com>
2024-10-15 19:20:46 -0400

    Add files via upload

---
+++ php/contactform-process.php
@@ -0,0 +1,59 @@
+<?php
+$errorMSG = "";
+
+if (empty($_POST["name"])) {
+    $errorMSG = "Name is required ";
+} else {
+    $name = $_POST["name"];
+}
+
+if (empty($_POST["email"])) {
+    $errorMSG = "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
+
+if (empty($_POST["message"])) {
+    $errorMSG = "Message is required ";
+} else {
+    $message = $_POST["message"];
+}
+
+if (empty($_POST["terms"])) {
+    $errorMSG = "Terms is required ";
+} else {
+    $terms = $_POST["terms"];
+}
+
+$EmailTo = "juank.febol@gmail.com";
+$Subject = "Mensaje rápido de la web FIPAZ";
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+$Body .= "Terms: ";
+$Body .= $terms;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "enviado con éxito";
+}else{
+    if($errorMSG == ""){
+        echo "Hubo un error intente más tarde :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+?>
